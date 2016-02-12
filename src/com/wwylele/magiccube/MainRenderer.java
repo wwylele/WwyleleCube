76f982e57af969d40570ac24a23ee0c738dff2a5
wwylele <wwylele@gmail.com>
2016-02-12 12:23:16 +0200

    pull down the API level

--- src/com/wwylele/magiccube/MainRenderer.java
+++ src/com/wwylele/magiccube/MainRenderer.java
@@ -30,11 +30,39 @@ public class MainRenderer implements Renderer {
 
     }
 
+    // Matrix.perspectiveM for low API
+    public static void perspectiveM(float[] m, int offset,
+            float fovy, float aspect, float zNear, float zFar) {
+        float f = 1.0f / (float) Math.tan(fovy * (Math.PI / 360.0));
+        float rangeReciprocal = 1.0f / (zNear - zFar);
+
+        m[offset + 0] = f / aspect;
+        m[offset + 1] = 0.0f;
+        m[offset + 2] = 0.0f;
+        m[offset + 3] = 0.0f;
+
+        m[offset + 4] = 0.0f;
+        m[offset + 5] = f;
+        m[offset + 6] = 0.0f;
+        m[offset + 7] = 0.0f;
+
+        m[offset + 8] = 0.0f;
+        m[offset + 9] = 0.0f;
+        m[offset + 10] = (zFar + zNear) * rangeReciprocal;
+        m[offset + 11] = -1.0f;
+
+        m[offset + 12] = 0.0f;
+        m[offset + 13] = 0.0f;
+        m[offset + 14] = 2.0f * zFar * zNear * rangeReciprocal;
+        m[offset + 15] = 0.0f;
+    }
+
     @Override
     public void onSurfaceChanged(GL10 unused, int width, int height) {
         GLES20.glViewport(0, 0, width, height);
         float ratio = (float) width / height;
-        Matrix.perspectiveM(mtxProj, 0, 45, ratio, 1, 100);
+        // Matrix.perspectiveM(mtxProj, 0, 45, ratio, 1, 100);
+        perspectiveM(mtxProj, 0, 45, ratio, 1, 100);
     }
 
     @Override
